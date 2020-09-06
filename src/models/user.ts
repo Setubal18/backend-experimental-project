fec930596cd886c54bdbb7f0acceac96563d282e
Leonardo Setubal <setubal43@gmail.com>
2020-09-06 17:18:28 -0300

    feat: added model of user and token generator

---
+++ src/models/user.ts
@@ -0,0 +1,63 @@
+import { Document, Schema, model, Mongoose } from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2'
+
+export interface userInterface extends Document {
+	name: string,
+	email: {
+		email: string,
+		createdAt?: Date,
+		updatedAt?: Date
+	},
+	password?: {
+		password: string,
+		createdAt?: Date,
+		updatedAt?: Date
+
+	},
+	createdAt?: Date,
+	updatedAt?: Date
+
+	getReturnJson(): any
+}
+
+const UserSchema = new Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	email: new Schema({
+		email: {
+			type: String,
+			lowercase: true,
+			required: true,
+		},
+	},
+		{
+			_id: false,
+			timestamps: true,
+		}),
+	password: new Schema({
+		password: {
+			type: String,
+			required: true
+		},
+	},
+		{
+			_id: false,
+			timestamps: true,
+		}),
+})
+
+UserSchema.plugin(mongoosePaginate)
+
+UserSchema.methods.getReturnJson = function () {
+	const returnJson = this.toJSON()
+	delete returnJson.password.password;
+
+
+	return returnJson
+}
+
+
+export const User = model<userInterface>('user', UserSchema);
