c45c3782105ced983ba4e3eb37459025506f7aa4
Leonardo Setubal <setubal43@gmail.com>
2020-09-09 18:18:19 -0300

    fix porta

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -3,5 +3,5 @@ export const db = {
 	uri: process.env.DB_URI || ''
 }
 
-export const port_serve = process.env.PORT_SERVER || 3333
+export const port_serve = process.env.PORT_SERVER || 3000
 export const secret = process.env.SECRET || ''
